626b886738a35e1b1b9e24b543b1b05a1b861aa5
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2016-07-01 13:11:13 +0100

    added a concatenate fn that takes care of newaxis if some elements are missing a dim

--- core/my_arrays.py
+++ core/my_arrays.py
@@ -12,6 +12,15 @@ def cell2mat(input_arg,axis,use_copy=True):
         curr=copy.deepcopy(curr)
     return curr
 
+
+def my_concatenate(input_arg,axis,newaxis_idx=0):
+    
+    ndims=[x.ndim for x in input_arg]
+    max_ndim=np.max(ndims)
+    input_arg2=[x if x.ndim==max_ndim else np.expand_dims(x, newaxis_idx) for x in input_arg]
+    
+    return np.concatenate(tuple(input_arg2),axis)
+
 def test_cell2mat():
     x=cell2mat([np.arange(0,9).reshape(3,3),2*np.arange(0,9).reshape(3,3)],1)
     print(x)
