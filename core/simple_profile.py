2c8fa6d22828e4db0eda630ed0ea21b7e966bc95
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2016-07-19 15:05:51 +0100

    added simple timer/profiler using the yield-finally construct

---
+++ core/simple_profile.py
@@ -0,0 +1,12 @@
+import time
+from contextlib import contextmanager
+@contextmanager
+
+def timer(label):
+    start = time.time()
+    try:
+        yield
+    finally:
+        end = time.time()
+        print('%s: %0.6f' % (label, end-start))
+
